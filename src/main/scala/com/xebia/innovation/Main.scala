03c94944bfb45e1ad9ba011de831ca2984894e84
marcus <mmartina@xebia.com>
2015-06-26 14:49:21 +0200

    reformatted a bit

--- src/main/scala/com/xebia/innovation/Main.scala
+++ src/main/scala/com/xebia/innovation/Main.scala
@@ -6,15 +6,14 @@ import com.xebia.innovation.utils.Table2D.{ Separator, AbstractRow, Row }
 import com.xebia.innovation.utils.YPosition._
 import com.xebia.innovation.utils._
 import oscar.linprog.modeling._
-import oscar.linprog._
 import oscar.algebra._
 
 object Main extends App {
 
   optimize
 
-  def optimize = {
-    val squareSize = 4
+  def optimize {
+    val squareSize = 3
     val size = squareSize * squareSize
     val Lines = 0 until size
     val Columns = 0 until size
@@ -27,30 +26,21 @@ object Main extends App {
 
     maximize(sum(Lines, Columns, Numbers) { (number, l, c) ⇒ x(number)(l)(c) })
 
-    /* Every number, in every line should be chosen only once */
-    for (
-      n ← Numbers;
-      l ← Lines
-    ) add(sum(Columns)(c ⇒ x(n)(l)(c)) == 1)
+    for (n ← Numbers) {
 
-    /* Every number, in every column should be chosen only once */
-    for (
-      n ← Numbers;
-      c ← Columns
-    ) add(sum(Lines)(l ⇒ x(n)(l)(c)) == 1)
+      /* Every number, in every line should be chosen only once */
+      for (l ← Lines) add(sum(Columns)(c ⇒ x(n)(l)(c)).==(1))
 
-    /* One number should be assigned to every (line,column) positiion */
-    for (
-      l ← Lines;
-      c ← Columns
-    ) add(sum(Numbers)(n ⇒ x(n)(l)(c)) == 1)
+      /* Every number, in every column should be chosen only once */
+      for (c ← Columns) add(sum(Lines)(l ⇒ x(n)(l)(c)).==(1))
+
+      /* One number should be assigned to every (line,column) positiion */
+      for (s ← List.range(0, size, squareSize); t ← List.range(0, size, squareSize))
+        add(sum((s until s + squareSize), (t until t + squareSize))((i, j) ⇒ x(n)(i)(j)).==(1))
+    }
 
     /* One number should be assigned to every (line,column) positiion */
-    for (
-      n ← Numbers;
-      s ← List.range(0, size, squareSize);
-      t ← List.range(0, size, squareSize)
-    ) add(sum((s until s + squareSize), (t until t + squareSize))((i, j) ⇒ x(n)(i)(j)) == 1)
+    for (l ← Lines; c ← Columns) add(sum(Numbers)(n ⇒ x(n)(l)(c)).==(1))
 
     start()
 
