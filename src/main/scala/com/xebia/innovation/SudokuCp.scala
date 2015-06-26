c7b482c7bc823ed79b43d419c2d4e46483536de5
Misja Alma <malma@xebia.com>
2015-06-26 14:09:50 +0200

    removed comment

--- src/main/scala/com/xebia/innovation/SudokuCp.scala
+++ src/main/scala/com/xebia/innovation/SudokuCp.scala
@@ -3,16 +3,6 @@ package com.xebia.innovation
 import oscar.cp.core._
 import oscar.cp.modeling.{add, start, _}
 
-/**
- * Find a number consisting of 9 digits in which each of the digits from
- * 1 to 9 appears only once. This number must also satisfy these divisibility requirements:
- * 1. The number should be divisible by 9.
- * 2. If the rightmost digit is removed, the remaining number should be divisible by 8.
- * 3. If the rightmost digit of the new number is removed, the remaining number should be divisible by 7.
- * 4. And so on, until there's only one digit (which will necessarily be divisible by 1).
- *
- *
- */
 object SudokuCp extends CPModel with App {
 
   val Block = 3
